0460bfbaa276d71084cbd2fe4f0082c3ddf703f2
SajalSharma4 <79101773+SajalSharma4@users.noreply.github.com>
2022-12-21 17:09:03 +0530

    Add files via upload

---
+++ LinkedList/SortedLinkedList/TestClass.java
@@ -0,0 +1,43 @@
+package LinkedList.SortedLinkedList;
+
+import java.util.Scanner;
+
+public class TestClass {
+    public static void main(String[] args) {
+        int choice, data;
+
+        Scanner scan = new Scanner(System.in);
+
+        SortedLinkedList List = new SortedLinkedList();
+        List.CreateList();
+
+        while (true) {
+            System.out.println("1.Display List");
+            System.out.println("2.Insert a new node");
+            System.out.println("3.Search");
+            System.out.println("4.Exit");
+            System.out.print("Enter your choice : ");
+            choice = scan.nextInt();
+            if (choice == 4)
+                break;
+
+            switch (choice) {
+                case 1:
+                    List.Display();
+                    break;
+                case 2:
+                    System.out.println("Enter the element to be inserted : ");
+                    data = scan.nextInt();
+                    List.InsertInOrder(data);
+                    break;
+                case 3:
+                    System.out.println("Enter the element to be searched : ");
+                    data = scan.nextInt();
+                    List.Search(data);
+                    break;
+                default:
+                    System.out.println("Wrong choice\n");
+            }
+        }
+    }
+}
