a70060a44bbc6b69c41cfce9ae3e43ba909df2be
SajalSharma4 <79101773+SajalSharma4@users.noreply.github.com>
2023-01-18 17:12:23 +0530

    Add files via upload

---
+++ Tree/Binary_Tree/Node.java
@@ -0,0 +1,13 @@
+package Tree.Binary_Tree;
+
+public class Node {
+    Node lchild;
+    char info;
+    Node rchild;
+
+    public Node(char ch) {
+        info = ch;
+        lchild = null;
+        rchild = null;
+    }
+}
