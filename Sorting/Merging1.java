72d0f81536b3abcdec9cc0c0f5904e363155aa86
SajalSharma4 <79101773+SajalSharma4@users.noreply.github.com>
2023-03-23 12:19:39 +0530

    Add files via upload

---
+++ Sorting/Merging1.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Merging1 {
+    private Merging1() {
+    }
+
+    public static void merge(int a[], int temp[], int low1, int up1, int low2, int up2) {
+        int i = low1;
+        int j = low2;
+        int k = low1;
+
+        while ((i <= up1) && (j <= up2)) {
+            if (a[i] <= a[j])
+                temp[k++] = a[i++];
+            else
+                temp[k++] = a[j++];
+        }
+
+        while (i <= up1)
+            temp[k++] = a[i++];
+
+        while (j <= up2)
+            temp[k++] = a[j++];
+    }
+
+    public static void main(String[] args) {
+        int i;
+        int[] a = { 1, 3, 5, 7, 2, 4, 6, 9, 11, 14 };
+        int[] temp = new int[20];
+
+        Scanner in = new Scanner(System.in);
+
+        merge(a, temp, 0, 3, 4, 9);
+
+        System.out.println("Array a is : ");
+        for (i = 0; i <= 9; i++)
+            System.out.print(a[i] + " ");
+        System.out.println();
+
+        System.out.println("Merged array temp is : ");
+        for (i = 0; i <= 9; i++)
+            System.out.print(temp[i] + " ");
+        System.out.println();
+        in.close();
+    }
+}
